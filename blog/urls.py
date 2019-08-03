c9bc66379121f18c8add673dcfed1cfca3f046d7
xolanisiqhelo <siqhelo25@gmail.com>
2019-08-03 12:52:55 +0200

    first commit for the status app

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from . import  views
+from .views import PostListView,PostDetailsView,PostCreateView,PostUpdateView,PostDeleteView,UserPostListView
+from django.urls import path
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailsView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about')
+
+]
